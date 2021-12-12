258e5148f2e5eb7d137870993f0fb512a9068d82
Akihiko <anakamuro@earthlink.net>
2021-12-12 03:23:48 -0500

    second commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,18 +1,15 @@
    
    async function getPhotographers() {
         // TODO : Replace with data from the JSON file
-       const response = await  fetch("./data/photographers.json");
-        const photographers = await response.json;
-    return ({
-            photographers: [...photographers]})
-    }
-   
+       return await(await fetch("./data/photographers.json")).json();
+   }
+       
    
     
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
+       
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
